4e0f0bc9a08b06b4aa80f4eb6233f84be9e7d595
horek <azenoxe@gmail.com>
2022-11-14 18:22:09 +0100

    feat: add base layout and homepage

--- library/urls.py
+++ library/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/register/", views.register_request, name="register")
